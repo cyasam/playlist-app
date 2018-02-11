1ac361047fddd33b20ae80092bd87e8d3d9ebdf6
cyasam <cyasam@gmail.com>
2018-02-12 02:54:06 +0300

    routing type changed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
